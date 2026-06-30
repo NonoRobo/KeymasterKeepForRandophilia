a2298432f8db477720790b5f96e6e4fef4a88d17
Nono Geulin <veryblueviolin@gmail.com>
2026-06-30 17:06:14 +0200

    LoL: add champions obj and role constraints

--- worlds/keymasters_keep/games/league_of_legends.py
+++ worlds/keymasters_keep/games/league_of_legends.py
@@ -11,7 +11,7 @@ from ..game_objective_template import GameObjectiveTemplate
 
 @dataclass
 class LeagueOfLegendsArchipelagoOptions:
-    pass
+    lol_unlocked_champions: LoLUnlockedChampions
 
 class LeagueOfLegendsGame(Game):
     name = "League of Legends"
@@ -22,6 +22,16 @@ class LeagueOfLegendsGame(Game):
 
     def optional_game_constraint_templates(self) -> List[GameObjectiveTemplate]:
         constraints = []
+
+        constraints.extend([
+            GameObjectiveTemplate(
+                label="Play as ROLE role.",
+                data={
+                    "ROLE": (lambda: self.lol_roles(), 1)
+                },
+            ),
+        ])
+
         return constraints
     
     def game_objective_templates(self) -> List[GameObjectiveTemplate]:
@@ -29,34 +39,231 @@ class LeagueOfLegendsGame(Game):
 
         game_objective_templates.extend([
             GameObjectiveTemplate(
+                label="Play a game with CHAMPION.",
+                data={
+                    "CHAMPION": (lambda: self.lol_unlocked_champions, 1)
+                },
+                is_time_consuming=False,
+                is_difficult=False,
+                weight=1,
+            ),
+        ])
+        wunit = sum(o.weight for o in game_objective_templates)
+
+        game_objective_templates.extend([
+            GameObjectiveTemplate(
                 label="Play a game.",
                 data={},
                 is_time_consuming=False,
                 is_difficult=False,
-                weight=3,
+                weight=3*wunit,
             ),
             GameObjectiveTemplate(
                 label="Perform a Double Kill.",
                 data={},
                 is_time_consuming=False,
                 is_difficult=False,
-                weight=1,
+                weight=wunit,
             ),
             GameObjectiveTemplate(
                 label="Destroy a tower.",
                 data={},
                 is_time_consuming=False,
                 is_difficult=False,
-                weight=1,
+                weight=wunit,
             ),
             GameObjectiveTemplate(
                 label="Steal an objective.",
                 data={},
                 is_time_consuming=False,
                 is_difficult=False,
-                weight=1,
+                weight=wunit,
             ),
         ])
 
         return game_objective_templates
+    
+    
+    @property
+    def lol_unlocked_champions(self) -> List[str]:
+        return self.archipelago_options.lol_unlocked_champions.value
+    
+    
+    @staticmethod
+    def lol_roles() -> List[str]:
+        return ["Top","Jungle","Mid","ADC","Support","Random"]
 
+
+class LoLUnlockedChampions(OptionList):
+    display_name = "[LoL] Unlocked champions"
+    valid_keys = {
+        "Aatrox",
+        "Ahri",
+        "Akali",
+        "Akshan",
+        "Alistar",
+        "Amumu",
+        "Anivia",
+        "Annie",
+        "Aphelios",
+        "Ashe",
+        "Aurelion Sol",
+        "Aurora",
+        "Azir",
+        "Bard",
+        "Bel'Veth",
+        "Blitzcrank",
+        "Brand",
+        "Braum",
+        "Briar",
+        "Caitlyn",
+        "Camille",
+        "Cassiopeia",
+        "Cho'Gath",
+        "Corki",
+        "Darius",
+        "Diana",
+        "Dr. Mundo",
+        "Draven",
+        "Ekko",
+        "Elise",
+        "Evelynn",
+        "Ezreal",
+        "Fiddlesticks",
+        "Fiora",
+        "Fizz",
+        "Galio",
+        "Gangplank",
+        "Garen",
+        "Gnar",
+        "Gragas",
+        "Graves",
+        "Gwen",
+        "Hecarim",
+        "Heimerdinger",
+        "Hwei",
+        "Illaoi",
+        "Irelia",
+        "Ivern",
+        "Janna",
+        "Jarvan IV",
+        "Jax",
+        "Jayce",
+        "Jhin",
+        "Jinx",
+        "K'Sante",
+        "Kai'Sa",
+        "Kalista",
+        "Karma",
+        "Karthus",
+        "Kassadin",
+        "Katarina",
+        "Kayle",
+        "Kayn",
+        "Kennen",
+        "Kha'Zix",
+        "Kindred",
+        "Kled",
+        "Kog'Maw",
+        "LeBlanc",
+        "Lee Sin",
+        "Leona",
+        "Lillia",
+        "Lissandra",
+        "Lucian",
+        "Lulu",
+        "Lux",
+        "Malphite",
+        "Malzahar",
+        "Maokai",
+        "Master Yi",
+        "Milio",
+        "Miss Fortune",
+        "Mordekaiser",
+        "Morgana",
+        "Naafiri",
+        "Nami",
+        "Nasus",
+        "Nautilus",
+        "Neeko",
+        "Nidalee",
+        "Nilah",
+        "Nocturne",
+        "Nunu",
+        "Olaf",
+        "Orianna",
+        "Ornn",
+        "Pantheon",
+        "Poppy",
+        "Pyke",
+        "Qiyana",
+        "Quinn",
+        "Rakan",
+        "Rammus",
+        "Rek'Sai",
+        "Rell",
+        "Renata Glasc",
+        "Renekton",
+        "Rengar",
+        "Riven",
+        "Rumble",
+        "Ryze",
+        "Samira",
+        "Sejuani",
+        "Senna",
+        "Seraphine",
+        "Sett",
+        "Shaco",
+        "Shen",
+        "Shyvana",
+        "Singed",
+        "Sion",
+        "Sivir",
+        "Skarner",
+        "Smolder",
+        "Sona",
+        "Soraka",
+        "Swain",
+        "Sylas",
+        "Syndra",
+        "Tahm Kench",
+        "Taliyah",
+        "Talon",
+        "Taric",
+        "Teemo",
+        "Thresh",
+        "Tristana",
+        "Trundle",
+        "Tryndamere",
+        "Twisted Fate",
+        "Twitch",
+        "Udyr",
+        "Urgot",
+        "Varus",
+        "Vayne",
+        "Veigar",
+        "Vel'Koz",
+        "Vex",
+        "Vi",
+        "Viego",
+        "Viktor",
+        "Vladimir",
+        "Volibear",
+        "Warwick",
+        "Wukong",
+        "Xayah",
+        "Xerath",
+        "Xin Zhao",
+        "Yasuo",
+        "Yone",
+        "Yorick",
+        "Yuumi",
+        "Zac",
+        "Zed",
+        "Zeri",
+        "Ziggs",
+        "Zilean",
+        "Zoe",
+        "Zyra",
+    }
+    default = valid_keys
